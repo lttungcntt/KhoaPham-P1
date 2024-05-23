9e43ce6a5450a54612d04fdca37751131bde90b5
Tyler <tung.le@legato.co>
2024-05-23 15:20:30 +0700

    feat: add IHomeDetail

--- lib/src/modules/home_detail/domain/entities/home_detail.dart
+++ lib/src/modules/home_detail/domain/entities/home_detail.dart
@@ -1,3 +1,16 @@
+import '../../../../common/enum/enum.dart';
+
 abstract class IHomeDetail {
   int get id;
+  String get name;
+  String get email;
+  String get phoneNumber;
+  DateTime get birthDate;
+  String get address;
+  String get city;
+  String get country;
+  Gender get gender;
+  List<String> get photos;
+  List<String> get videos;
+  List<String> get reels;
 }
